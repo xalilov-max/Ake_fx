642752c8e525d0e9be587413e0c75f13deec4f69
xalilov-max <xalilovbobur056@gmail.com>
2025-06-05 00:51:25 +0500

    postgres qoshildi

--- akefx/settings.py
+++ akefx/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'akefx.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'akefx_db',
+        'USER': 'akefx_user',
+        'PASSWORD': 'Boburjon07',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
